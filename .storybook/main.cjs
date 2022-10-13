46c68625d6876ee80597838aac4c004bcb412c4a
Weslen Martins <info@weslenmartins.com.br>
2022-10-13 20:47:02 -0300

    ci: Add publish Storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/study-design-system-storybook/'
+    }
+
+    return configType
   }
 }
